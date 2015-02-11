266f3c71c871baf79f8ca0265ef67c2170ff1856
Valdas Mistolis <valdas.mistolis@gmail.com>
2015-02-11 13:53:29 +0200

    initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Klavakrapstis\SolitaireBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('KlavakrapstisSolitaireBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ KlavakrapstisSolitaireBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Klavakrapstis\SolitaireBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KlavakrapstisSolitaireBundle extends Bundle
+{
+}
